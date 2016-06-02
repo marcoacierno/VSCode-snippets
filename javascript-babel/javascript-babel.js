f92275e7dfe44f205d2e6894aee6ad9acb218da3
Marco Acierno <marcoaciernoemail@gmail.com>
2016-06-02 18:46:27 +0200

    Add React boilerplate snippet

---
+++ javascript-babel/javascript-babel.js
@@ -0,0 +1,18 @@
+{
+    "React boilerplate": {
+        "prefix": "rc",
+        "body": [
+            "import React, {",
+            "   Component,",
+            "} from 'react';",
+            "",
+            "",
+            "export default class $1 extends Component {",
+            "   render() {",
+            "       return <div>Hello</div>;",
+            "   }",
+            "}"
+        ],
+        "description": "React class boilerplate"
+    }
+}
